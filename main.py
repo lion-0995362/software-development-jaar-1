cd7abb495038a23f93a7cfd4751030507668adf1
lion-0995362 <113017282+lion-0995362@users.noreply.github.com>
2022-09-14 14:31:42 +0200

    test

---
+++ main.py
@@ -0,0 +1,63 @@
+
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 // 2)
+# print(23.95 * 32.9)
+# print((23.95 * 37.7) + 8)
+
+# name = "Lion"
+# print(name)
+# street = "Zebistreet"
+# print(street)
+
+# print(f'the city of {city} has {residents} on the arcity = 'Rotterdam'
+# print(city)
+# type(city)
+
+# residents = 623652
+# print(residents)
+# type(residents)
+
+# area = 324.1
+# print(area)
+# type(area)
+
+people = 7861000000
+print(people)
+people = 7_861_000_000
+print(people)
+
+# meals = 3
+# people_eat = people * meals
+# print(people_eat)
+
+# days = 365
+# meals_per_year = people_eat * days
+# print(meals_per_year)
+
+# welke datatype is het?
+# type(meals_per_year)
+
+
+# name = input("What is your name? ")
+# print("Hello" + name)
+
+people = 'Dina'
+print(people)
+
+
+print(people)
+
+# gek 
+
+naam= 2+3
+print(naam)
+
+print(variable)
+
+variable="geld"
+
+
+
